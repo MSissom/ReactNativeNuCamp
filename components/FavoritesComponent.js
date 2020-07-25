ee9d1e0c5dd52232aa46195ffbfbd6d882ed1c27
Morgan Sissom <Masissom@gmail.com>
2020-07-25 11:06:02 -0400

    done with task 2 workshop, working correctly, experimenting with quasi incompatible NetInfo

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { FlatList, View, Text, Alert } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
-import { Loading } from './LoadingComponent';
+import Loading from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 import Swipeout from 'react-native-swipeout';
 import { deleteFavorite } from '../redux/ActionCreators';
